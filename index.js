313ca145dee83903fa8c25d6df864cbf905c7726
Gaurav Kondhare <kondharegaurav@gmail.com>
2020-05-31 23:06:30 +0530

    rbac: Add role based access control to secured api endpoints.
    Fixes #11. I have currently added all the necessary roles, however
    haven't added all the permissions and resources that maybe required in the 
    future. Resources are self made and specific to our project. Currently the only 
    resource that we are working on for now is the `profile` resource and hence only
    that has been added. Many more like `post`, `analytics` etc need to be added as 
    and when encountered in the future.
    
    All the information needed for the permissions ( updateOwn, readAny ...) can be 
    found in the npm module that we are using `accesscontrol`. More info can be 
    found out at `https://onury.io/accesscontrol/` on how to use the permissions.
    
    The rbac has been added as an express middleware, and hence can be passed to the
    express api route methods and can be used with ease. 
    `allowIfLoggedIn` checks if a user is logged in before further processing for 
    secured api endpoints.
    I have added a middleware for the whole express module which processes each 
    request and if has an `authorization` header it passes the loggedInUser as a 
    `res.local` var for the next middleware routes. This makes it easier to fetch 
    the info about who is the loggedInUser. 
    Modified the current api endpoints to work with the newly added rbac
    abstractions.
    
    Also added a `vscode` workspace settings so that everyone has the same
    codebase and doesn't change file line endings. Added recommendations
    for using the same extensions across.

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const JwtService = require("./services/JwtService");
+const UserService = require("./services/UserService");
+const Error = require("./models/Error");
+require("dotenv").config;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose
-    .connect(process.env.MONGO, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-    })
-    .then(() => console.log("Connected to mongodb"))
-    .catch((err) => console.log(err));
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Connected to mongodb"))
+  .catch((err) => console.log(err));
 
 const port = 3000;
 
-app.get("/", async (req, res) => {
-    //! Only for testing
+// Adds the loggedIn user to the response's local vars for easy access to
+// other routes.
+app.use(async (req, res, next) => {
+  if (req.headers["authorization"]) {
+    const accessToken = req.headers["authorization"];
     try {
-        const users = await require("./services/UserService").getAllUsers();
-        res.status(200).json(users);
+      const { userID } = JwtService.verify(accessToken);
+      res.locals.loggedInUser = await UserService.getUser(userID);
+      next();
     } catch (error) {
-        res.status(400).json(error);
+      return res.status(401).json(new Error("BAD_REQUEST", error.message));
     }
+  } else {
+    next();
+  }
 });
 
 app.use("/api/auth/", require("./routes/auth"));
 app.use("/api/users/", require("./routes/user"));
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
--- routes/user.js
+++ routes/user.js
@@ -3,281 +3,268 @@ const bcrypt = require("bcryptjs");
 const Error = require("../models/Error");
 const Joi = require("@hapi/joi");
 const jwtService = require("../services/JwtService");
-
 const userService = require("../services/UserService");
 
 /*
  * GET USER INFO
  * [Only loggedIn user can see their info. The userID of loggedIn user is stored in the JWT token itself]
  */
-router.get("/", async (req, res) => {
-    let userID = null;
+router.get(
+  "/",
+  userService.allowIfLoggedIn,
+  userService.hasAccessTo("readOwn", "profile"),
+  async (req, res) => {
+    userID = req.user.zprn;
     try {
-        userID = jwtService.verify(req.headers["authorization"]).userID;
+      const user = await userService.getUser(userID);
+      if (user == null) {
+        res.status(401).json(new Error("BAD_REQUEST", "No user found!"));
+      } else {
+        res.status(200).json({
+          code: "OK",
+          result: "SUCCESS",
+          user: {
+            zprn: user.zprn,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            department: user.department,
+          },
+        });
+      }
     } catch (error) {
-        res.status(403).send(new Error("FORBIDDEN", error.message));
-        return;
+      res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
     }
-    try {
-        const user = await userService.getUser(userID);
-        if (user == null) {
-            res.status(401).json(new Error("BAD_REQUEST", "No user found!"));
-        } else {
-            res.status(200).json({
-                code: "OK",
-                result: "SUCCESS",
-                user: {
-                    zprn: user.zprn,
-                    firstName: user.firstName,
-                    lastName: user.lastName,
-                    department: user.department,
-                },
-            });
-        }
-    } catch (error) {
-        res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
-    }
-});
+  }
+);
 
-/*
+/**
  * ADD NEW USER
+ * Todo : Check for specific type of roles or else reject the request
  */
 router.post("/add", async (req, res) => {
-    let { zprn, firstName, lastName, password, department, role } = req.body;
-    try {
-        const { error } = validateData(
-            zprn,
-            firstName,
-            lastName,
-            password,
-            department,
-            role
-        );
-        if (error != undefined) {
-            res.status(401).send(
-                new Error("BAD_REQUEST", error.details[0].message)
-            );
-        } else {
-            const salt = bcrypt.genSaltSync(10);
-            const hashedPassword = bcrypt.hashSync(password, salt);
-            await userService.addUser(
-                zprn,
-                firstName,
-                lastName,
-                hashedPassword,
-                department,
-                role
-            );
-            res.status(201).json({
-                code: "CREATED",
-                msg: "User added!",
-                result: "SUCCESS",
-            });
-        }
-    } catch (error) {
-        res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
+  let { zprn, firstName, lastName, password, department, role } = req.body;
+  try {
+    const { error } = validateData(
+      zprn,
+      firstName,
+      lastName,
+      password,
+      department,
+      role
+    );
+    if (error != undefined) {
+      res.status(401).send(new Error("BAD_REQUEST", error.details[0].message));
+    } else {
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      await userService.addUser(
+        zprn,
+        firstName,
+        lastName,
+        hashedPassword,
+        department,
+        role
+      );
+      res.status(201).json({
+        code: "CREATED",
+        msg: "User added!",
+        result: "SUCCESS",
+      });
     }
+  } catch (error) {
+    res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
+  }
 });
 
-/*
+/**
  * USER: UPDATE PASSWORD
+ * Todo : Check for valid password and password same after validating that
+ *        the user actually exists. OR Maybe we do not need to validate if the
+ *        if the user actually exists because are previous middleware actually
+ *        checks for the loggedInUser and even passes the loggedInUser into
+ *        the `req.user` var which by transitivity means the user actually exists
+ *        and we just need to update the password without checking for it.
  */
-router.patch("/update/password", async (req, res) => {
-    let userID = null;
+router.patch(
+  "/update/password",
+  userService.allowIfLoggedIn,
+  userService.hasAccessTo("updateOwn", "profile"),
+  async (req, res) => {
+    userID = req.user.zprn;
     try {
-        userID = jwtService.verify(req.headers["authorization"]).userID;
-    } catch (error) {
-        res.status(403).send(new Error("FORBIDDEN", error.message));
-        return;
-    }
-    try {
-        const { oldPassword, newPassword } = req.body;
-        const { error } = validatePassword(oldPassword, newPassword);
-        if (error !== undefined) {
-            res.status(401).send(
-                new Error("BAD_REQUEST", error.details[0].message)
+      const { oldPassword, newPassword } = req.body;
+      const { error } = validatePassword(oldPassword, newPassword);
+      if (error !== undefined) {
+        res
+          .status(401)
+          .send(new Error("BAD_REQUEST", error.details[0].message));
+      } else {
+        if (oldPassword === newPassword) {
+          res
+            .status(401)
+            .send(
+              new Error(
+                "BAD_REQUEST",
+                "New password should be different from previous password."
+              )
             );
-        } else {
-            if (oldPassword === newPassword) {
-                res.status(401).send(
-                    new Error(
-                        "BAD_REQUEST",
-                        "New password should be different from previous password."
-                    )
-                );
-                return;
-            }
-            try {
-                const user = await userService.getUser(userID);
-                if (user == null) {
-                    res.status(401).send(
-                        new Error("BAD_REQUEST", "No user found.")
-                    );
-                } else {
-                    const match = bcrypt.compareSync(
-                        oldPassword,
-                        user.password
-                    );
-                    if (match) {
-                        const hashedPassword = bcrypt.hashSync(newPassword, 10);
-                        await userService.updateUser({
-                            zprn: userID,
-                            password: hashedPassword,
-                        });
-                        res.status(200).json({
-                            code: "OK",
-                            result: "SUCCESS",
-                            msg: "Password updated!",
-                        });
-                    } else {
-                        res.status(401).send(
-                            new Error("BAD_REQUEST", "Incorrect Password")
-                        );
-                    }
-                }
-            } catch (err) {
-                res.status(401).send(new Error("BAD_REQUEST", err.message));
-            }
+          return;
         }
+        try {
+          const user = await userService.getUser(userID);
+          if (user == null) {
+            res.status(401).send(new Error("BAD_REQUEST", "No user found."));
+          } else {
+            const match = bcrypt.compareSync(oldPassword, user.password);
+            if (match) {
+              const hashedPassword = bcrypt.hashSync(newPassword, 10);
+              await userService.updateUser({
+                zprn: userID,
+                password: hashedPassword,
+              });
+              res.status(200).json({
+                code: "OK",
+                result: "SUCCESS",
+                msg: "Password updated!",
+              });
+            } else {
+              res
+                .status(401)
+                .send(new Error("BAD_REQUEST", "Incorrect Password"));
+            }
+          }
+        } catch (err) {
+          res.status(401).send(new Error("BAD_REQUEST", err.message));
+        }
+      }
     } catch (error) {
-        res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
+      res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
     }
-});
+  }
+);
 
 /*
  * USER: UPDATE PROFILE DETAILS
  * [firstName,lastName & department only]
  */
-router.post("/update/profile", async (req, res) => {
-    let userID = null;
+router.post(
+  "/update/profile",
+  userService.allowIfLoggedIn,
+  userService.hasAccessTo("updateOwn", "profile"),
+  async (req, res) => {
+    userID = req.user.zprn;
     try {
-        userID = jwtService.verify(req.headers["authorization"]).userID;
+      if (JSON.stringify(req.body) === "{}") {
+        res.status(401).send(new Error("BAD_REQUEST", "Insufficient data."));
+      } else {
+        const { firstName, lastName, department } = req.body;
+        await userService.updateUser({
+          zprn: userID,
+          firstName: firstName,
+          lastName: lastName,
+          department: department,
+        });
+        res.status(200).json({
+          code: "OK",
+          result: "SUCCESS",
+          msg: "User updated!",
+        });
+      }
     } catch (error) {
-        res.status(403).send(new Error("FORBIDDEN", error.message));
-        return;
+      res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
     }
-    try {
-        if (JSON.stringify(req.body) === "{}") {
-            res.status(401).send(
-                new Error("BAD_REQUEST", "Insufficient data.")
-            );
-        } else {
-            const { firstName, lastName, department } = req.body;
-            await userService.updateUser({
-                zprn: userID,
-                firstName: firstName,
-                lastName: lastName,
-                department: department,
-            });
-            res.status(200).json({
-                code: "OK",
-                result: "SUCCESS",
-                msg: "User updated!",
-            });
-        }
-    } catch (error) {
-        res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
-    }
-});
+  }
+);
 /*
  * AUTHORITY: UPDATE USER DETAILS [ALL]
  */
-router.post("/update/profile/:userID", async (req, res) => {
+router.post(
+  "/update/profile/:userID",
+  userService.allowIfLoggedIn,
+  userService.hasAccessTo("updateAny", "profile"),
+  async (req, res) => {
     try {
-        const role = jwtService.verify(req.headers["authorization"]).role;
-        if (role === "student") {
-            res.status(403).send(new Error("FORBIDDEN", "Access denied"));
-            return;
-        }
-    } catch (error) {
-        res.status(403).send(new Error("FORBIDDEN", error.message));
-        return;
-    }
-    try {
-        if (JSON.stringify(req.body) === "{}") {
-            res.status(401).send(
-                new Error("BAD_REQUEST", "Required fields not provided")
-            );
-        } else {
-            const password = req.body.password;
-            const hashedPassword =
-                password != undefined
-                    ? bcrypt.hashSync(password, 10)
-                    : password;
-            await userService.updateUser({
-                zprn: Number(req.params.userID),
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                password: hashedPassword,
-                department: req.body.department,
-                role: req.body.role,
-            });
-            res.status(200).json({
-                code: "OK",
-                result: "SUCCESS",
-                msg: "User updated!",
-            });
-        }
-    } catch (error) {
-        res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
-    }
-});
-
-router.delete("/:userID", async (req, res) => {
-    let role = null;
-    try {
-        role = jwtService.verify(req.headers["authorization"]).role;
-        if (role !== "moderator") {
-            res.status(403).send(new Error("FORBIDDEN", "Access denied"));
-            return;
-        }
-    } catch (error) {
-        res.status(403).send(new Error("FORBIDDEN", error.message));
-        return;
-    }
-    try {
-        const user = await userService.deleteUser(Number(req.params.userID));
-        res.status(200).json({
-            code: "OK",
-            result: "SUCCESS",
-            deleted: user,
+      if (JSON.stringify(req.body) === "{}") {
+        res
+          .status(401)
+          .send(new Error("BAD_REQUEST", "Required fields not provided"));
+      } else {
+        const password = req.body.password;
+        const hashedPassword =
+          password != undefined ? bcrypt.hashSync(password, 10) : password;
+        await userService.updateUser({
+          zprn: Number(req.body.zprn),
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          password: hashedPassword,
+          department: req.body.department,
+          role: req.body.role,
         });
+        res.status(200).json({
+          code: "OK",
+          result: "SUCCESS",
+          msg: "User updated!",
+        });
+      }
     } catch (error) {
-        res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
+      res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
     }
-});
+  }
+);
+
+/**
+ * Todo : Check if user is already deleted and return an error if is already
+ *        deleted.
+ */
+router.delete(
+  "/:userID",
+  userService.allowIfLoggedIn,
+  userService.hasAccessTo("deleteAny", "profile"),
+  async (req, res) => {
+    try {
+      const user = await userService.deleteUser(Number(req.params.userID));
+      res.status(200).json({
+        code: "OK",
+        result: "SUCCESS",
+        deleted: user,
+      });
+    } catch (error) {
+      res.status(500).send(new Error("INTERNAL_SERVER_ERROR", error.message));
+    }
+  }
+);
 
 /*
  * VALIDATION FUNCTIONS
  */
 
 function validateData(zprn, firstName, lastName, password, department, role) {
-    const schema = Joi.object({
-        zprn: Joi.string().required().length(7).regex(/^\d+$/),
-        firstName: Joi.string().required(),
-        lastName: Joi.string().required(),
-        password: Joi.string().required().min(6),
-        department: Joi.string().required().min(3),
-        role: Joi.string().required(),
-    });
+  const schema = Joi.object({
+    zprn: Joi.string().required().length(7).regex(/^\d+$/),
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    password: Joi.string().required().min(6),
+    department: Joi.string().required().min(3),
+    role: Joi.string().required(),
+  });
 
-    return schema.validate({
-        zprn: zprn,
-        firstName: firstName,
-        lastName: lastName,
-        password: password,
-        department: department,
-        role: role,
-    });
+  return schema.validate({
+    zprn: zprn,
+    firstName: firstName,
+    lastName: lastName,
+    password: password,
+    department: department,
+    role: role,
+  });
 }
 
 function validatePassword(old, newPassword) {
-    const schema = Joi.object({
-        oldPassword: Joi.string().required(),
-        newPassword: Joi.string().required().min(6),
-    });
+  const schema = Joi.object({
+    oldPassword: Joi.string().required(),
+    newPassword: Joi.string().required().min(6),
+  });
 
-    return schema.validate({ oldPassword: old, newPassword: newPassword });
+  return schema.validate({ oldPassword: old, newPassword: newPassword });
 }
 
 module.exports = router;

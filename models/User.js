5304e0494bb20413c7ec329ec70d2c5daa86cecc
Faiyaz Mujawar <mujawar.faiyaz.aziz@gmail.com>
2021-03-24 15:42:29 +0530

    issues: implemented blick user
    In this PR,
    - implemented functionality to block users based on the number of code of conduct violations by them.
    - Modified the User Schema to accommodate the violations(id of respective issue) by them.
    - Modify the Issue Schema  to accommodate the users reporting the issue.
    - Modified the filterIssues function to delete the reporters to prevent identity leak.

--- models/User.js
+++ models/User.js
@@ -1,38 +1,42 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    zprn: {
-        type: Number,
-        required: true,
-        unique: true,
-    },
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    department: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        required: true,
-    },
-    isDisabled: {
-        type: Boolean,
-    },
-    refreshToken: {
-        type: String,
-        unique: true,
-    },
+  zprn: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  violations: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+  },
+  isDisabled: {
+    type: Boolean,
+  },
+  refreshToken: {
+    type: String,
+    unique: true,
+  },
 });
 
 module.exports = mongoose.model("User", UserSchema);
